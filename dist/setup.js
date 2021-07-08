c87c942c8cce02d8467c125a932ce75c654688f9
John Conley <john.conley@nordicsemi.no>
2021-07-08 14:26:13 -0400

    fix: inject keys when not present

--- dist/setup.js
+++ dist/setup.js
@@ -7,5 +7,9 @@ exports.default = (withConfigDir) => {
     // Provide environment variables before other scripts are executed
     process.env.MOCK_DYNAMODB_PORT = port.toString();
     process.env.MOCK_DYNAMODB_ENDPOINT = `http://localhost:${port}`;
+    if (process.env.AWS_PROFILE == null && process.env.AWS_ACCESS_KEY_ID == null && process.env.AWS_SECRET_ACCESS_KEY == null) {
+        process.env.AWS_ACCESS_KEY_ID = 'coolkey';
+        process.env.AWS_SECRET_ACCESS_KEY = 'secert';
+    }
 };
 //# sourceMappingURL=setup.js.map
--- src/setup.ts
+++ src/setup.ts
@@ -8,4 +8,9 @@ export default (withConfigDir: string): void => {
   // Provide environment variables before other scripts are executed
   process.env.MOCK_DYNAMODB_PORT = port.toString();
   process.env.MOCK_DYNAMODB_ENDPOINT = `http://localhost:${port}`;
+
+  if (process.env.AWS_PROFILE == null && process.env.AWS_ACCESS_KEY_ID == null && process.env.AWS_SECRET_ACCESS_KEY == null) {
+    process.env.AWS_ACCESS_KEY_ID = 'coolkey';
+    process.env.AWS_SECRET_ACCESS_KEY = 'secert';
+  }
 };
